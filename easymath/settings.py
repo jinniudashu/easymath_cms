cffdc6f8a17619b138e9fc8636efd6241a517c05
jinniudashu <jinniudashu@gmail.com>
2022-12-19 12:10:40 +0800

    Deleted TEMPLATES->DIRS

--- easymath/settings.py
+++ easymath/settings.py
@@ -56,9 +56,7 @@ ROOT_URLCONF = 'easymath.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
