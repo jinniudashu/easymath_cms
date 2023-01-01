015ec5116a9a6bf279be6e96e46e5f558a55310b
jinniudashu <jinniudashu@gmail.com>
2023-01-01 21:00:54 +0800

    Added drf_yasg Restful API Doc tools

--- courses/urls.py
+++ courses/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import courses_list, units_list, lessons_list, unit_lessons_list, lesson_detail, exercises_list
 
 urlpatterns = [
-    path('courses/', courses_list, name='courses'),  # 所有系列课
-    path('courses/<int:pk>/units/', units_list, name='units'),  # 某系列课的所有单元
-    path('courses/<int:pk>/lessons/', lessons_list, name='lessons'),  # 某系列课的所有视频课
-    path('courses/<int:cpk>/units/<int:upk>/lessons/', unit_lessons_list, name='unit_lessons'),  # 某系列课的某单元的所有视频课
-    path('lessons/<int:pk>/', lesson_detail, name='lesson_detail'), # 某视频课的详细信息
-    path('lessons/<int:pk>/exercises/', exercises_list, name='exercises'), # 某视频课的所有练习
+    path('/', courses_list, name='courses'),  # 所有系列课
+    path('/<int:pk>/units/', units_list, name='units'),  # 某系列课的所有单元
+    path('/<int:pk>/lessons/', lessons_list, name='lessons'),  # 某系列课的所有视频课
+    path('/<int:cpk>/units/<int:upk>/lessons/', unit_lessons_list, name='unit_lessons'),  # 某系列课的某单元的所有视频课
+    path('/lessons/<int:pk>/', lesson_detail, name='lesson_detail'), # 某视频课的详细信息
+    path('/lessons/<int:pk>/exercises/', exercises_list, name='exercises'), # 某视频课的所有练习
 ]
--- easymath/urls.py
+++ easymath/urls.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    # 具体定义详见 [Swagger/OpenAPI 规范](https://swagger.io/specification/#infoObject)
+    openapi.Info(
+        title="Easymath API",
+        default_version='v1',
+        description="Easymath API 文档",
+        terms_of_service="",
+        contact=openapi.Contact(email="test@test.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    # public 表示文档完全公开, 无需针对用户鉴权
+    public=True,
+    # 可以传递 drf 的 BasePermission
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('courses/', include('courses.urls')),
+    path('courses', include('courses.urls')),
+
+    # drf_yasg
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-spec'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
