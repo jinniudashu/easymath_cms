6202a4e09fce3ba33495bc997bc4b1ed64a44c6f
jinniudashu <jinniudashu@gmail.com>
2023-01-02 11:19:48 +0800

    Added empty files into static for drf-yasg

--- easymath/settings.py
+++ easymath/settings.py
@@ -167,6 +167,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = []
 
